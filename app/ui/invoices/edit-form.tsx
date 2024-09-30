256f5370b24f74e90931760aeb9848e853f98eab
Livia <livia_macedo89@yahoo.com.br>
2024-09-30 11:25:27 -0300

    Error Messages UpdateInvoice - chapter 14

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,7 +9,8 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
-import { updateInvoice } from '@/app/lib/actions';
+import { updateInvoice, State } from '@/app/lib/actions';
+import { useActionState } from 'react';
 
 export default function EditInvoiceForm({
   invoice,
@@ -18,10 +19,18 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
-  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+  const initialState: State = { message: null, errors: {}}
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id)
+  const [state, formAction] = useActionState(updateInvoiceWithId, initialState)
 
   return (
-    <form action={updateInvoiceWithId}>
+    <form action={formAction}>
+      {/* Exibir mensagem geral de erro */}
+      {state.message && (
+        <div className="mb-4 text-red-500">
+          {state.message}
+        </div>
+      )}
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -11,7 +11,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
   // Debounced search handler
   const handleSearch = useDebouncedCallback((term: string) => {
-    console.log(`Searching... ${term}`);
+    // console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
     params.set('page', '1')
     if (term) {
